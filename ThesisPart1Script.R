94e0c22f0636ea36ca0af7a334862c21baf35689
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2019-09-07 12:57:28 -0700

    Forget to hit save before. THIS is the initial look at June growth data.

--- ThesisPart1Script.R
+++ ThesisPart1Script.R
@@ -3,19 +3,18 @@
 
 #Clear the environment
 rm(list=ls())
-#Load the data
-mydata<-read.csv("Part1.csv")
+#Load June growth data
+mydata<-read.csv("JunePart1.csv")
 View(mydata)
+
+#load libraries
+library(tidyverse)
+
 #Renaming Date column because it transferred weird
 names(mydata)[1]<-"Date"
 View(mydata)
 
-library(ggplot2)
+#Very bad plot of all growth measurements over time
 ggplot(data=mydata,aes(x = Date, y= Densityx10000))+geom_line()+geom_point()
-
-library(tidyr)
-library(dplyr)
-df<-mydata %>%
-  select(Date,Genotype,Temp)%>%
-  gather(key="variable", value="value",-Date)
-View(df)
+#Looks like it starts to flatten at about 6/12/2019
+#Need to clean up data to have "days" instead of "dates"
