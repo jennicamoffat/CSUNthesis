3de03d4934d93edf293bf9cda53e1b57fa47b2b2
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2019-09-16 07:48:58 -0700

    Made growth curves of each genotype at all three temps.

--- Thesis_GrowthCurves_Cultures.R
+++ Thesis_GrowthCurves_Cultures.R
@@ -19,3 +19,55 @@ View(mydata)
 ggplot(data=mydata,aes(x = Date, y= Densityx10000))+geom_line()+geom_point()
 #Looks like it starts to flatten at about 6/12/2019
 #Need to clean up data to have "days" instead of "dates"
+
+#Clear the environment
+rm(list=ls())
+#Load July growth data
+mydata<-read.csv("JulyGrowthPart1_r.csv")
+View(mydata)
+
+#Very bad plot of all growth measurements over time
+ggplot(data=mydata,aes(x = Day, y=Density.10.4))+geom_point()
+
+
+#Making temp a factor rather than numeric
+mydata$Temp<-as.factor(mydata$Temp)
+
+#Growth curves
+CCMP2458Growth<-ggplot(subset(mydata, Genotype == "CCMP2458"), aes(x=Day, y=Densityx10000, group=Temp, color=Temp))+
+  geom_point()+
+  theme_classic()+
+  ggtitle("Growth Curve CCMP2458")+
+  geom_smooth()
+CCMP2458Growth
+
+
+CCMP2464Growth<-ggplot(subset(mydata, Genotype == "CCMP2464"), aes(x=Day, y=Densityx10000, group=Temp, color=Temp))+
+  geom_point()+
+  theme_classic()+
+  ggtitle("Growth Curve CCMP2464")+
+  geom_smooth()
+CCMP2464Growth
+
+
+FLCassGrowth<-ggplot(subset(mydata, Genotype == "FLCass"), aes(x=Day, y=Densityx10000, group=Temp, color=Temp))+
+  geom_point()+
+  theme_classic()+
+  ggtitle("Growth Curve FLCass")+
+  geom_smooth()
+FLCassGrowth
+
+RT362Growth<-ggplot(subset(mydata, Genotype == "RT362"), aes(x=Day, y=Densityx10000, group=Temp, color=Temp))+
+  geom_point()+
+  theme_classic()+
+  ggtitle("Growth Curve RT362")+
+  geom_smooth()
+RT362Growth
+
+
+KB8Growth<-ggplot(subset(mydata, Genotype == "KB8"), aes(x=Day, y=Densityx10000, group=Temp, color=Temp))+
+  geom_point()+
+  theme_classic()+
+  ggtitle("Growth Curve KB8")+
+  geom_smooth()
+KB8Growth
