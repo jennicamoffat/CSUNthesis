c146d7a1a7026865981de1a48056a9b0429fa175
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2020-02-16 18:44:06 -0800

    Cassiopea Polyp script

---
+++ Thesis_Polyp_Script.R
@@ -0,0 +1,59 @@
+#Cassiopea Polyp Data
+#Created by Jennica Moffat 02162020
+
+#Clear the environment
+rm(list=ls())
+#Load PnR data
+mydata<-read.csv("Thesis_PolypData_Summary.csv")
+View(mydata)
+
+#load libraries
+library(tidyverse)
+library(car)
+
+#Graph of ephyra production including dead ones
+
+#New data frame without Apo (since no ephyra)
+NoApoData <- subset(mydata, Genotype != "Aposymbiotic")
+
+TotalEphyra <- NoApoData %>%
+  group_by(Genotype, Temp) %>%
+  summarize(mean=mean(Total.Ephyra.Produced, na.rm=TRUE), SE=sd(Total.Ephyra.Produced, na.rm=TRUE)/sqrt(length(na.omit(Total.Ephyra.Produced))))
+TotalEphyra
+
+TotalEphyraPlot<-ggplot(TotalEphyra, aes(x=Genotype, y=mean, fill=factor(Temp), group=factor(Temp)))+  #basic plot
+  theme_bw()+ #Removes grey background
+  theme(axis.text.x=element_text(color="black", size=14), axis.text.y=element_text(face="bold", color="black", size=12), axis.title.x = element_text(face="bold", color="black", size=16), axis.title.y = element_text(face="bold", color="black", size=16),panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  geom_bar(stat="identity", position="dodge", size=0.6) + #determines the bar width
+  geom_errorbar(aes(ymax=mean+SE, ymin=mean-SE), stat="identity", position=position_dodge(width=0.9), width=0.1)+  #adds error bars
+  labs(x="Genotype", y="Average Ephyra per polyp", fill="Temperature")+  #labels the x and y axes
+  ggtitle("Average Ephyra produced per Polyp (dead included)")+
+  scale_fill_manual(values = c("skyblue3", "darkgoldenrod2", "brown3"), labels=c("26°C", "30°C", "32°C"))
+TotalEphyraPlot
+
+TotalEphyraPlot+ggsave("Graphs/Polyps/EphyraProduced_deadincluded.pdf", width=11, height=6.19, dpi=300, unit="in")
+
+
+#New dataframe without dead
+NoApoNoDeadData <- subset(NoApoData, Survive.to.End == "Yes")
+View(NoApoNoDeadData)
+
+TotalEphyraNoDead <- NoApoNoDeadData %>%
+  group_by(Genotype, Temp) %>%
+  summarize(mean=mean(Total.Ephyra.Produced, na.rm=TRUE), SE=sd(Total.Ephyra.Produced, na.rm=TRUE)/sqrt(length(na.omit(Total.Ephyra.Produced))))
+TotalEphyraNoDead
+
+TotalEphyraNoDeadPlot<-ggplot(TotalEphyraNoDead, aes(x=Genotype, y=mean, fill=factor(Temp), group=factor(Temp)))+  #basic plot
+  theme_bw()+ #Removes grey background
+  theme(axis.text.x=element_text(color="black", size=14), axis.text.y=element_text(face="bold", color="black", size=12), axis.title.x = element_text(face="bold", color="black", size=16), axis.title.y = element_text(face="bold", color="black", size=16),panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  geom_bar(stat="identity", position="dodge", size=0.6) + #determines the bar width
+  geom_errorbar(aes(ymax=mean+SE, ymin=mean-SE), stat="identity", position=position_dodge(width=0.9), width=0.1)+  #adds error bars
+  labs(x="Genotype", y="Average Ephyra per polyp", fill="Temperature")+  #labels the x and y axes
+  ggtitle("Average Ephyra produced per Polyp (dead removed)")+
+  scale_fill_manual(values = c("skyblue3", "darkgoldenrod2", "brown3"), labels=c("26°C", "30°C", "32°C"))
+TotalEphyraNoDeadPlot
+
+TotalEphyraNoDeadPlot+ggsave("Graphs/Polyps/EphyraProduced_deadremoved.pdf", width=11, height=6.19, dpi=300, unit="in")
+
+
+
