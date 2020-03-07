9bfe8c41c84206b6da3bfb038f2aa55aac9d9e3f
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2020-03-07 13:34:43 -0800

    Polyp data, stats for ephyra and time to strobilation.

--- Thesis_Polyp_Script.R
+++ Thesis_Polyp_Script.R
@@ -4,14 +4,15 @@
 #Clear the environment
 rm(list=ls())
 #Load PnR data
-mydata<-read.csv("Thesis_PolypData_Summary.csv")
+mydata<-read.csv("Data/Thesis_PolypData_Summary.csv")
 View(mydata)
 
 #load libraries
 library(tidyverse)
 library(car)
 
-#Graph of average ephyra production including dead ones####
+#GRAPHS####
+#Graph of total average ephyra production including dead ones####
 #New data frame without Apo (since no ephyra)
 NoApoData <- subset(mydata, Genotype != "Aposymbiotic")
 
@@ -37,7 +38,7 @@ TotalEphyraPlot+ggsave("Graphs/Polyps/EphyraProduced_deadincluded.pdf", width=11
 NoApoNoDeadData <- subset(NoApoData, Survive.to.End == "Yes")
 View(NoApoNoDeadData)
 
-#Average Total Ephyra Produced####
+#Average Total Ephyra Produced#
 TotalEphyraNoDead <- NoApoNoDeadData %>%
   group_by(Genotype, Temp) %>%
   summarize(mean=mean(Total.Ephyra.Produced, na.rm=TRUE), SE=sd(Total.Ephyra.Produced, na.rm=TRUE)/sqrt(length(na.omit(Total.Ephyra.Produced))))
@@ -54,6 +55,28 @@ TotalEphyraNoDeadPlot<-ggplot(TotalEphyraNoDead, aes(x=Genotype, y=mean, fill=fa
 TotalEphyraNoDeadPlot
 
 TotalEphyraNoDeadPlot+ggsave("Graphs/Polyps/EphyraProduced_deadremoved.pdf", width=11, height=6.19, dpi=300, unit="in")
+
+#Average Total Ephyra Produced as percent that inoculated 
+InoculatedData <- subset(NoApoNoDeadData, Days.to.Inoculation != "NA")
+
+EphyraifInoc <- InoculatedData %>%
+  group_by(Genotype, Temp) %>%
+  summarize(mean=mean(Total.Ephyra.Produced, na.rm=TRUE), SE=sd(Total.Ephyra.Produced, na.rm=TRUE)/sqrt(length(na.omit(Total.Ephyra.Produced))))
+EphyraifInoc
+
+EphyraifInocPlot<-ggplot(EphyraifInoc, aes(x=Genotype, y=mean, fill=factor(Temp), group=factor(Temp)))+  #basic plot
+  theme_bw()+ #Removes grey background
+  theme(axis.text.x=element_text(color="black", size=14), axis.text.y=element_text(face="bold", color="black", size=12), axis.title.x = element_text(face="bold", color="black", size=16), axis.title.y = element_text(face="bold", color="black", size=16),panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  geom_bar(stat="identity", position="dodge", size=0.6) + #determines the bar width
+  geom_errorbar(aes(ymax=mean+SE, ymin=mean-SE), stat="identity", position=position_dodge(width=0.9), width=0.1)+  #adds error bars
+  labs(x="Genotype", y="Average Ephyra per polyp", fill="Temperature")+  #labels the x and y axes
+  ggtitle("Average Ephyra produced per Polyp (if inoculated)")+
+  scale_fill_manual(values = c("skyblue3", "darkgoldenrod2", "brown3"), labels=c("26°C", "30°C", "32°C"))
+EphyraifInocPlot
+
+EphyraifInocPlot+ggsave("Graphs/Polyps/EphyraifInoc.pdf", width=11, height=6.19, dpi=300, unit="in")
+
+
 
 #Time to strobilation####
 #Removing the NA's, which are the ones that never strobilated. Losing part of the story. 
@@ -75,6 +98,15 @@ DaystoStrobNoDeadPlot
 
 DaystoStrobNoDeadPlot+ggsave("Graphs/Polyps/DaystoStrob_deadremoved.pdf", width=11, height=6.19, dpi=300, unit="in")
 
+#Boxplot of data to see distribution
+ggplot(NoApoNoDeadData, aes(x=Genotype, y=Days.to.Strobilation, fill=factor(Temp)))+  #basic plot
+  theme_bw()+
+  geom_boxplot()+
+  labs(x="Genotype", y="Avg days to strobilation", fill="Temperature")+  #labels the x and y axes
+  ggtitle("Average Days to Strobilation (dead removed)")+
+  scale_fill_manual(values = c("skyblue3", "darkgoldenrod2", "brown3"), labels=c("26°C", "30°C", "32°C"))
+#Automatically removes all NA's values, so graph is just of those that did strobilate
+
 #Time to inoculation (same issue as strobilation)####
 DaystoInocNoDead <- NoApoNoDeadData %>%
   group_by(Genotype, Temp) %>%
@@ -91,7 +123,7 @@ DaystoInocNoDeadPlot<-ggplot(DaystoInocNoDead, aes(x=Genotype, y=mean, fill=fact
   scale_fill_manual(values = c("skyblue3", "darkgoldenrod2", "brown3"), labels=c("26°C", "30°C", "32°C"))
 DaystoInocNoDeadPlot
 
-DaystoInocNoDeadPlot+ggsave("Graphs/Polyps/DaystoStrob_deadremoved.pdf", width=11, height=6.19, dpi=300, unit="in")
+DaystoInocNoDeadPlot+ggsave("Graphs/Polyps/DaystoInoc_deadremoved.pdf", width=11, height=6.19, dpi=300, unit="in")
 
 #time to ephyra####
 #same issue as above
@@ -172,3 +204,77 @@ BudPlot
 
 BudPlot+ggsave("Graphs/Polyps/BudPlot_deadremoved.pdf", width=11, height=6.19, dpi=300, unit="in")
 
+
+
+#STATS####
+#Total average ephyra production####
+#Removing aposymbiotic
+NoApoData <- subset(mydata, Genotype != "Aposymbiotic")
+View(NoApoData)
+
+
+#Genotype (fixed) and Temperature (fixed) on dependent variables (total ephyra production)
+
+#Make model
+Ephyramodel<-aov(Total.Ephyra.Produced~Genotype*Temp, data=NoApoData)
+#Check assumptions
+plot(Ephyramodel)
+model1res<-resid(Ephyramodel)
+qqp(model1res, "norm")
+#I think it's normal enough. A few outliers(?) 338, 361, 290, and 313
+
+#Run model
+EphyraAnova<-anova(Ephyramodel)
+EphyraAnova
+#Yay! Significant interaction between Genotype and Temp (unsurprisingly)
+#Genotype:Temp, P=0.0008394 ***
+
+#Time to strobilation####
+
+#I need to remove the NA's
+StrobilatedData <- subset(NoApoData, Days.to.Strobilation != "NA")
+View(StrobilatedData)
+
+#Make model
+Strobmodel<-aov(Days.to.Strobilation~Genotype*Temp, data=StrobilatedData)
+#Check assumptions
+plot(Strobmodel)
+Strobmodelres<-resid(Strobmodel)
+qqp(Strobmodelres, "norm")
+#Not great. A few outliers: 337, 318, 186, and 194
+
+#transform
+StrobilatedData$logDays<-log(StrobilatedData$Days.to.Strobilation)
+logStrobmodel<-aov(logDays~Genotype*Temp, data=StrobilatedData)
+logStrobmodelres<-resid(logStrobmodel)
+qqp(logStrobmodelres, "norm")
+#Better,let's log one more time
+
+StrobilatedData$loglogDays<-log(StrobilatedData$logDays)
+loglogStrobmodel<-aov(loglogDays~Genotype*Temp, data=StrobilatedData)
+loglogStrobmodelres<-resid(loglogStrobmodel)
+qqp(loglogStrobmodelres, "norm")
+#Slightly better, but a lot of points still just above CI's 
+
+
+#Square root
+StrobilatedData$sqrtDays<-sqrt(StrobilatedData$Days.to.Strobilation)
+sqrtStrobmodel<-aov(sqrtDays~Genotype*Temp, data=StrobilatedData)
+sqrtStrobmodelres<-resid(sqrtStrobmodel)
+qqp(sqrtStrobmodelres, "norm")
+#Barely changed. 
+
+#Using model of double logged data
+anova(loglogStrobmodel)
+#interesting...no interaction. Geno and temp are sig on their own, but P=0.5 for interaction.
+
+#Time to inoculation
+
+
+#Time to ephyra
+
+
+#Survival
+
+
+#Bud production
