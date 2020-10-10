dadc2d9973609cd365aca32f2d393c5a072ed417
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2020-10-10 11:45:10 -0700

    Strobilation graphs - proportion, bargraph, boxplot.

--- Thesis_Polyp_graphs.R
+++ Thesis_Polyp_graphs.R
@@ -12,6 +12,7 @@ library(car)
 mydata<-read.csv("Data/Thesis_PolypData_Summary.csv")
 mydata<-mydata%>%
   mutate_if(is.character,as.factor)
+mydata$Temp<-as.factor(mydata$Temp)
 
 #Aposymbiotic removed (for developmental timing and ephyra)
 NoApoData <- mydata %>%
@@ -27,6 +28,7 @@ Developed.data<-NoApoData%>%
          Ephyra=ifelse(is.na(Days.to.Ephyra), "No", "Yes"))
 
 #GRAPHS#
+#Total ephyra production####
 #Bargraph of total average ephyra production including dead ones####
 #New data frame without Apo (since no ephyra)
 NoApoData <- mydata %>%
@@ -182,62 +184,74 @@ EphyraifInocPlot+ggsave("Graphs/Polyps/EphyraifInoc.pdf", width=11, height=6.19,
 
 
 #Time to strobilation####
-#Removing the NA's, which are the ones that never strobilated. Losing part of the story. 
-#How to manage?
 
 #First, let's see how many even got to strobilation
 total.strob<-Developed.data%>%
   group_by(Genotype, Temp, Strob)%>%
   tally()
-ggplot(total.strob, aes(fill=Strob, y=n, x=Temp))+
-  geom_bar(position="stack", stat="identity")+
-  facet_wrap(~Genotype)
+
+pal<-c("#DFADE1", "#859A51") #green, pink (potentially not colorblind friendly)
+pal<-c("#DFADE1", "#2c7fb8")
+pal<-c("#2c7fb8","#7fcdbb") #blue green
+
+strob.prop<-ggplot(total.strob, aes(x=Temp, y=n, fill=Strob))+  #basic plot
+  theme_minimal()+
+  theme(axis.text.x=element_text(color="black", size=11), axis.text.y=element_text(color="black", size=11), axis.title.x = element_text(color="black", size=13),strip.text.x = element_text(size = 11, colour = "black"))+
+  geom_bar(position=position_stack(), stat="identity")+
+  scale_fill_manual(values=pal)+
+  labs(x="Temperature (°C)", y="", fill="Strobilated")+#labels the x and y axes
+  scale_y_continuous(expand=c(0,0), limits=c(0,25))+
+  facet_grid(~Genotype)
+strob.prop
+strob.prop+ggsave("Graphs/FinalGraphs/Strob.Proportion.png", width=10, height=5)
+
+pal<-c("#79CFDB", "#859A51", "#DFADE1") #blue, green, pink (potentially not colorblind friendly)
+strob.prop2<-ggplot(total.strob, aes(x=Genotype, y=n, fill=Temp, group=Temp, alpha=Strob))+  #basic plot
+  theme_bw()+
+  geom_bar(position="dodge", stat="identity")+
+  scale_fill_manual(values=pal, labels = c("26°C", "30°C", "32°C"))
+strob.prop2
+#Ugh, this is so close to what I want, but I can't do dodge and stacked position in a single graph.
 
 
-DaystoStrobNoDead <- NoApoNoDeadData %>%
+#Bargraph of days to reach strobilation
+DaystoStrob <- NoApoData %>%
   group_by(Genotype, Temp) %>%
   summarize(mean=mean(Days.to.Strobilation, na.rm=TRUE), SE=sd(Days.to.Strobilation, na.rm=TRUE)/sqrt(length(na.omit(Days.to.Strobilation))))
 
-DaystoStrobNoDeadPlot<-ggplot(DaystoStrobNoDead, aes(x=Genotype, y=mean, fill=factor(Temp), group=factor(Temp)))+  #basic plot
+pal<-c("#ac8eab", "#f2cec7", "#c67b6f") #purples
+DaystoStrobBar<-ggplot(DaystoStrob, aes(x=Genotype, y=mean, fill=factor(Temp), group=factor(Temp)))+  #basic plot
   theme_bw()+ #Removes grey background
   scale_y_continuous(expand=c(0,0), limits=c(0, 25))+
-  theme(plot.title = element_text(face = "bold", size=16), axis.text.x=element_text(color="black", size=12), axis.text.y=element_text(color="black", size=12), axis.title.x = element_text(color="black", size=16), axis.title.y = element_text(color="black", size=16),panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  theme(axis.text.x=element_text(color="black", size=11), 
+        axis.text.y=element_text(color="black", size=12), 
+        axis.title.x = element_text(color="black", size=16), 
+        axis.title.y = element_text(color="black", size=16),
+        panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
   geom_bar(stat="identity", position="dodge", size=0.6) + #determines the bar width
   geom_errorbar(aes(ymax=mean+SE, ymin=mean-SE), stat="identity", position=position_dodge(width=0.9), width=0.1)+  #adds error bars
-  labs(x="Symbiont Strain", y="Number of days", fill="Temperature")+  #labels the x and y axes
-  ggtitle("Average Days to Strobilation")+
-  scale_fill_manual(values = c("skyblue3", "darkgoldenrod2", "brown3"), labels=c("26°C", "30°C", "32°C"))
-DaystoStrobNoDeadPlot
+  labs(x="Symbiont Genotype", y="Days to Strobilation", fill="Temperature")+  #labels the x and y axes
+  scale_fill_manual(values=pal, labels = c("26°C", "30°C", "32°C"))
+DaystoStrobBar
+DaystoStrobBar+ggsave("Graphs/FinalGraphs/DaystoStrob.png", width=8, height=5)
 
-DaystoStrobNoDeadPlot+ggsave("Graphs/Polyps/DaystoStrob_deadremoved.png", width=8, height=5)
-
-#Boxplot of data to see distribution
-ggplot(NoApoNoDeadData, aes(x=Genotype, y=Days.to.Strobilation, fill=factor(Temp)))+  #basic plot
-  theme_bw()+
-  geom_boxplot()+
-  labs(x="Genotype", y="Avg days to strobilation", fill="Temperature")+  #labels the x and y axes
-  ggtitle("Average Days to Strobilation (dead removed)")+
-  scale_fill_manual(values = c("skyblue3", "darkgoldenrod2", "brown3"), labels=c("26°C", "30°C", "32°C"))
-#Automatically removes all NA's values, so graph is just of those that did strobilate
-
-
+#Boxplot
 strob.boxplot<-NoApoData%>%
   ggplot(aes(x=Genotype, y=Days.to.Strobilation, fill=Temp))+
   geom_boxplot()+
   theme_bw()+
   geom_jitter(color="black", size=0.5, alpha=0.7)+
-  scale_x_discrete(name = "Genotype") +
+  scale_x_discrete(name = "Symbiont Genotype") +
   scale_y_continuous(name = "Days to Strobilation")+
-  ggtitle("Days to Strobilation")+
-  theme(plot.title = element_text(face="bold"), 
-        axis.text.x=element_text(size=10), 
-        axis.text.y=element_text(size=10), 
-        axis.title.y = element_text(face="bold", size=12), 
-        axis.title.x = element_text(face="bold", size=12))+
+  theme(axis.text.x=element_text(color="black", size=11), 
+        axis.text.y=element_text(color="black", size=12), 
+        axis.title.x = element_text(color="black", size=16), 
+        axis.title.y = element_text(color="black", size=16),
+        panel.grid.major=element_blank(), panel.grid.minor=element_blank())+
   labs(fill="Temperature")+
-  scale_fill_manual(values = c("#79CFDB", "#859A51", "#DFADE1"), labels=c("26°C", "30°C","32°C"))
-
-strob.boxplot+ggsave("Graphs/Polyps/DaystoStrob.boxplot.png", width=10, height=5)
+  scale_fill_manual(values = c("#ac8eab", "#f2cec7", "#c67b6f"), labels=c("26°C", "30°C","32°C"))
+strob.boxplot
+strob.boxplot+ggsave("Graphs/FinalGraphs/DaystoStrob.boxplot.final.png", width=8, height=5)
 
 #Just temp, not geno
 DaystoStrobNoDeadTemp <- NoApoNoDeadData %>%
@@ -580,207 +594,3 @@ BudsGeno<-ggplot(BudDataGeno, aes(x=Genotype, y=mean))+  #basic plot
   labs(x="Temperature", y="Number of Buds")+  #labels the x and y axes
   ggtitle("Average Bud Production (no dead)")
 BudsGeno+ggsave("Graphs/Polyps/BudsGeno.png", width=10, height=5)
-
-#STATS####
-#Clear the environment
-rm(list=ls())
-#Load data
-mydata<-read.csv("Data/Thesis_PolypData_Summary.csv")
-mydata$Temp<-as.factor(mydata$Temp)
-View(mydata)
-
-#Total average ephyra production####
-#Removing aposymbiotic
-NoApoData <- subset(mydata, Genotype != "Aposymbiotic")
-View(NoApoData)
-
-#Genotype (fixed) and Temperature (fixed) on dependent variables (total ephyra production)
-
-#Make model
-Ephyramodel<-aov(Total.Ephyra.Produced~Genotype*Temp*Plate, random="Plate", data=NoApoData)
-#Check assumptions
-plot(Ephyramodel)
-model1res<-resid(Ephyramodel)
-qqp(model1res, "norm")
-#I think it's normal enough. A few outliers(?) 338, 361, 290, and 313
-
-#Run model
-EphyraAnova<-anova(Ephyramodel)
-EphyraAnova
-#Yay! Significant interaction between Genotype and Temp (unsurprisingly)
-#Genotype:Temp, P=0.003441 ***
-
-#Time to strobilation####
-
-#I need to remove the NA's
-StrobilatedData <- subset(NoApoData, Days.to.Strobilation != "NA")
-View(StrobilatedData)
-
-#Make model
-Strobmodel<-aov(Days.to.Strobilation~Genotype*Temp, data=StrobilatedData)
-#Check assumptions
-plot(Strobmodel)
-Strobmodelres<-resid(Strobmodel)
-qqp(Strobmodelres, "norm")
-#Not great. A few outliers: 337, 318, 186, and 194
-
-#transform
-StrobilatedData$logDays<-log(StrobilatedData$Days.to.Strobilation)
-logStrobmodel<-aov(logDays~Genotype*Temp, data=StrobilatedData)
-logStrobmodelres<-resid(logStrobmodel)
-qqp(logStrobmodelres, "norm")
-#Better,let's log one more time
-
-StrobilatedData$loglogDays<-log(StrobilatedData$logDays)
-loglogStrobmodel<-aov(loglogDays~Genotype*Temp, data=StrobilatedData)
-loglogStrobmodelres<-resid(loglogStrobmodel)
-qqp(loglogStrobmodelres, "norm")
-#Slightly better, but a lot of points still just above CI's 
-
-
-#Square root
-StrobilatedData$sqrtDays<-sqrt(StrobilatedData$Days.to.Strobilation)
-sqrtStrobmodel<-aov(sqrtDays~Genotype*Temp, data=StrobilatedData)
-sqrtStrobmodelres<-resid(sqrtStrobmodel)
-qqp(sqrtStrobmodelres, "norm")
-#Barely changed. 
-
-#Using model of double logged data
-anova(loglogStrobmodel)
-#Interaction = 0.03549*
-
-
-
-#Time to inoculation####
-#I need to remove the NA's
-InocData <- subset(NoApoData, Days.to.Inoculation != "NA")
-View(InocData)
-
-#Make model
-Inocmodel<-aov(Days.to.Inoculation~Genotype*Temp, data=InocData)
-#Check assumptions
-plot(Inocmodel)
-Inocmodelres<-resid(Inocmodel)
-qqp(Inocmodelres, "norm")
-#Ew. That's pretty bad. 
-
-InocData$logIDays<-log(InocData$Days.to.Inoculation)
-logInocmodel<-aov(logIDays~Genotype*Temp, data=InocData)
-logInocmodelres<-resid(logInocmodel)
-qqp(logInocmodelres, "norm")
-#Ugh, possibly worse. 
-
-#double log
-InocData$loglogIDays<-log(InocData$logIDays)
-loglogInocmodel<-aov(loglogIDays~Genotype*Temp, data=InocData)
-loglogInocmodelres<-resid(loglogInocmodel)
-qqp(loglogInocmodelres, "norm")
-#Oh that is better
-
-#triple log??
-InocData$trplogIDays<-log(InocData$loglogIDays)
-trplogInocmodel<-aov(trplogIDays~Genotype*Temp, data=InocData)
-trplogInocmodelres<-resid(trplogInocmodel)
-qqp(trplogInocmodelres, "norm")
-#Idk man. No.
-
-hist(InocData$trplogIDays)
-
-#Square root
-InocData$sqrtIDays<-sqrt(InocData$Days.to.Inoculation)
-sqrtInocmodel<-aov(sqrtIDays~Genotype*Temp, data=InocData)
-sqrtInocmodelres<-resid(sqrtInocmodel)
-qqp(sqrtInocmodelres, "norm")
-#No. 
-
-#Fourth root
-InocData$fourthrtIDays<-sqrt(InocData$sqrtIDays)
-fourthrtInocmodel<-aov(fourthrtIDays~Genotype*Temp, data=InocData)
-fourthrtInocmodelres<-resid(fourthrtInocmodel)
-qqp(fourthrtInocmodelres, "norm")
-#No.
-
-#I think I have to do a GLMM. Later....
-
-#Time to ephyra####
-#I need to remove the NA's
-TimetoEphyraData <- subset(NoApoData, Days.to.Ephyra != "NA")
-View(TimetoEphyraData)
-
-#Make model
-TEphyramodel<-aov(Days.to.Ephyra~Genotype*Temp, data=TimetoEphyraData)
-#Check assumptions
-plot(TEphyramodel)
-TEphyramodelres<-resid(TEphyramodel)
-qqp(TEphyramodelres, "norm")
-#Not too bad, but needs transforming. 
-
-hist(TimetoEphyraData$loglogEDays)
-
-#Log
-TimetoEphyraData$logEDays<-log(TimetoEphyraData$Days.to.Ephyra)
-logEphyraDaysmodel<-aov(logEDays~Genotype*Temp, data=TimetoEphyraData)
-logEphyraDaysmodelres<-resid(logEphyraDaysmodel)
-qqp(logEphyraDaysmodelres, "norm")
-#Almost. 
-
-#Double log
-TimetoEphyraData$loglogEDays<-log(TimetoEphyraData$logEDays)
-loglogEphyraDaysmodel<-aov(loglogEDays~Genotype*Temp, data=TimetoEphyraData)
-loglogEphyraDaysmodelres<-resid(loglogEphyraDaysmodel)
-qqp(loglogEphyraDaysmodelres, "norm")
-#Mehhhhhh close enough?
-anova(loglogEphyraDaysmodel)
-#Interaction is not significant. This one makes sense. 
-#Genot and Temp on their own P=<2e-16
-#Interaction P=0.3658
-
-#Survival####
-#To get a rate I need to tally the number that survived, divided by 24 because that's how 
-#many each group started with
-Survival<- mydata%>%group_by(Genotype, Temp, Plate, WellNum)%>%
-  tally(Survive.to.End == "Yes")
-View(Survival)
-#1 is yes survived, 0 is no did not survive
-#So average should be the survival rate?
-Survivalmodel<-aov(n~Genotype*Temp, data=Survival)
-plot(Survivalmodel)
-
-#It's bi-model. So I think I need to do something other than anova.
-#I need to do a chi-squared test I think
-
-#To see survival overall, not separated by geno or temp
-table(Survival$n)
-prop.table(table(Survival$n))
-
-chisq.test(Survival$n, Survival$Genotype)
-#can't do interactions with chi-squared
-
-#Cochran-Mantel-Haenszel test
-mantelhaen.test(Survival$n, Survival$Temp, Survival$Genotype)
-#Order matters
-
-
-#Okay forget all that. I just need to do a log-linear analysis (G-test or chi-square)
-
-#Bud production####
-Budmodel<-aov(Total.Buds~Genotype*Temp, data=mydata)
-plot(Budmodel)
-Budres<-resid(Budmodel)
-qqp(Budres, "norm")
-#Beautifully normal
-
-anova(Budmodel)
-#interaction P=0.002318**
-#Second time I run it, interaction = 0.1904
-
-NoDeadData<-subset(mydata, Survive.to.End == "Yes")
-View(NoDeadData)
-
-BudmodelNodead<-aov(Total.Buds~Genotype*Temp, data=NoDeadData)
-BudNoDeadres<-resid(BudmodelNodead)
-qqp(BudNoDeadres, "norm")
-#Still normal
-anova(BudmodelNodead)
-#Interaction P=0.001066
-TukeyHSD(aov(Budmodel))
